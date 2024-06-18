bbf2aa802ee5ebaad4e11a103357240d35d373b1
Joe Diragi <joseph.diragi@icloud.com>
2024-06-18 16:27:19 -0400

    Add support for basic config in spm plugin

--- Plugins/RswiftGenerateInternalResources/RswiftGenerateInternalResources.swift
+++ Plugins/RswiftGenerateInternalResources/RswiftGenerateInternalResources.swift
@@ -8,6 +8,18 @@
 import Foundation
 import PackagePlugin
 
+struct RSwiftConfig: Codable {
+    enum Generator: String, Codable {
+        case image, string, color
+        case file, font, nib
+        case segue, storyboard, reuseIdentifier
+        case entitlements, info, id
+    }
+    
+    let generators: [Generator]
+    // TODO: More options?
+}
+
 @main
 struct RswiftGenerateInternalResources: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
@@ -29,7 +41,24 @@ struct RswiftGenerateInternalResources: BuildToolPlugin {
 
         let bundleSource = target.kind == .generic ? "module" : "finder"
         let description = "\(target.kind) module \(target.name)"
-
+        
+        var additionalArguments: [String] = []
+        if let files = try? FileManager.default.contentsOfDirectory(atPath: target.directory.string) {
+            configCheck: if let config = files.first(where: {$0.contains("rswift.json") }) {
+                guard let url = URL(string: "file://\(config)"),
+                        let fileContents = try? Data(contentsOf: url),
+                        let config = try? JSONDecoder().decode(RSwiftConfig.self, from: fileContents) else {
+                    break configCheck
+                }
+                let generators = config.generators.map(\.rawValue).joined(separator: ",")
+                additionalArguments += ["--generators", "\(generators)"]
+            }
+            
+            if let ignore = files.first(where: { $0.contains(".rswiftignore") }) {
+                additionalArguments += ["--rswiftignore", ignore]
+            }
+        }
+        
         return [
             .buildCommand(
                 displayName: "R.swift generate resources for \(description)",
@@ -38,7 +67,7 @@ struct RswiftGenerateInternalResources: BuildToolPlugin {
                     "generate", rswiftPath.string,
                     "--input-type", "input-files",
                     "--bundle-source", bundleSource,
-                ] + inputFilesArguments,
+                ] + inputFilesArguments + additionalArguments,
                 outputFiles: [rswiftPath]
             ),
         ]
